138d1d42ff1ff021501b3d33a12becac0841e78a
Ben Governali <bgovern93@gmail.com>
2023-01-17 14:30:46 -0800

    added amplify authentication flow

---
+++ eventbuddy/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "eventbuddy5af52ca0": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
