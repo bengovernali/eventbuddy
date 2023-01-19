427537c601072d7cae52df4c7204823f7243d9b0
Ben Governali <bgovern93@gmail.com>
2023-01-19 13:52:19 -0800

    having home component check if a token or hash exist, and if not navigate to root

--- eventbuddy/src/routes/home.jsx
+++ eventbuddy/src/routes/home.jsx
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from "react";
 import Header from "../components/headers";
 import { Box } from "@mui/system";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+  const navigate = useNavigate()
   const [token, setToken] = useState("");
   
   useEffect(() => {
     const hash = window.location.hash;
     let token = window.localStorage.getItem("token");
-    if (!token && hash) {
+    if (!token && !hash) {
+      navigate("/")
+    }
+    else if (!token && hash) {
       token = hash
         .substring(1)
         .split("&")
